d1330862970b2a24485fb4f095e2b7faaa1ca770
ANSHUNISHU <nishant7joshi7@gmail.com>
2024-05-10 12:11:48 +0530

    changes in app>page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Hero from "@/components/{main}/LandingPage/Home/Hero"
+import Hero from "@/Components/{main}/LandingPage/Home/Hero";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default page
+export default page;
